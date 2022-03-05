cddf2fb342bba55ce6476cb3396b072bbc2d1a00
realcodykarunas <101003379+realcodykarunas@users.noreply.github.com>
2022-03-05 16:03:36 -0600

    Backend working state. Link model resolvers wired up.

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./src/schema");
 const resolvers = require("./src/resolvers");
+const models = require("./models");
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
-  // context: ({ req }) => {
-  //   console.log("req: ", req);
-  // }
-  // // context: { models }
+  resolvers,
+  context: { models }
 });
 
 server.listen().then(({ url }) => {
