a0a2c291eabcddf8aadba68ef4522951ec0946ac
realcodykarunas <101003379+realcodykarunas@users.noreply.github.com>
2022-03-05 16:59:36 -0600

    Accepted the query and shortned the url to create a payload. Need to save to db and make sure values are not null or undefined.

--- index.js
+++ index.js
@@ -2,11 +2,38 @@ const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./src/schema");
 const resolvers = require("./src/resolvers");
 const models = require("./models");
+const slugify = require("slugify");
+const shortUrl = require("node-url-shortener");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: { models }
+  context: (req) => {
+    console.log("Body: ", req.req.body);
+    const { variables } = req.req.body;
+    console.log(variables);
+
+    let shortnedUrl = "";
+    shortUrl.short(`${variables.url}/${variables.slug}`, (err, url) => {
+      shortnedUrl = url;
+      console.log(shortnedUrl);
+    });
+
+    const linkToSave = {
+      url: shortnedUrl,
+      slug: slugify(variables.slug)
+    };
+
+    console.log(linkToSave);
+    return { linkToSave };
+
+    /**
+     * TODO:
+     * - Format Slug
+     * - Format Final payload
+     * - Save to DB
+     */
+  }
 });
 
 server.listen().then(({ url }) => {
